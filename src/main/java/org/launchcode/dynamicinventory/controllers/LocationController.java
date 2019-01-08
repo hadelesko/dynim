16ecc0be72f86cb2dce97fcb88624499257cd5ed
Koomi <koomiadeh@gmail.com>
2019-01-08 10:53:29 -0600

    seach by name, id,... by eflow,place specific material,...

--- src/main/java/org/launchcode/dynamicinventory/controllers/LocationController.java
+++ src/main/java/org/launchcode/dynamicinventory/controllers/LocationController.java
@@ -115,79 +115,81 @@ public class LocationController {
         model.addAttribute("locations", locations);
         return "location/editLocation";
     }
+
     @RequestMapping(value = "id={locationId}")
-    public String schowMaterial(Model model, @PathVariable int locationId){
-        List<Location>foundListOfLocation=new ArrayList<>();
-        Location foundLocation=locDao.findByLocationId(locationId);
+    public String schowMaterial(Model model, @PathVariable int locationId) {
+        List<Location> foundListOfLocation = new ArrayList<>();
+        Location foundLocation = locDao.findByLocationId(locationId);
         boolean existLocation;
-        if(foundLocation==null){
-            existLocation=false;
-            model.addAttribute("existLocation",existLocation);
-        }else{
-            existLocation=true;
-            model.addAttribute("existLocation",existLocation);
+        if (foundLocation == null) {
+            existLocation = false;
+            model.addAttribute("existLocation", existLocation);
+        } else {
+            existLocation = true;
+            model.addAttribute("existLocation", existLocation);
             foundListOfLocation.add(foundLocation);
         }
         model.addAttribute("foundListOfLocation", foundListOfLocation);
 
         //model.addAttribute("alllocations",locDao.findAll());
-        model.addAttribute("title", "Result of the location with id = "+locationId);
+        model.addAttribute("title", "Result of the location with id = " + locationId);
         return "location/singleLocationShow";
     }
+
     // This function aimed to close the gap between the available stock of each material and its stored quantity
     // It can come that the sum of quantities of the stored material in different places does not match the
     // total available quantity of the material in the system. THe stapler has maybe omitted to relocate the
     // material after the reception or forget to associate e new location for the new material
-    @RequestMapping(value="task")
-    public String locationstask(Model model){
-        List <MMaterial>listOfMaterials=new ArrayList<>();
+    @RequestMapping(value = "task")
+    public String locationstask(Model model) {
+        List<MMaterial> listOfMaterials = new ArrayList<>();
         listOfMaterials.addAll(matDao.findAll());
-        List<String>taskListOfMaterialNeedingRelocation=new ArrayList<>();
+        List<String> taskListOfMaterialNeedingRelocation = new ArrayList<>();
 
 
-        for(MMaterial material:listOfMaterials){
+        for (MMaterial material : listOfMaterials) {
             //get the stock of the material
-            double availableStock=material.getStock();
+            double availableStock = material.getStock();
             // Get the list of locations of this material if these exist
-            List<Location>locationsForhisMaterial=locDao.findByMaterial(material);
-            double locatedStockOfThisMaterial=0;
-            List<String>actuellocationsnameforthematerial=new ArrayList<>();
-            for(Location location:locationsForhisMaterial){
+            List<Location> locationsForhisMaterial = locDao.findByMaterial(material);
+            double locatedStockOfThisMaterial = 0;
+            List<String> actuellocationsnameforthematerial = new ArrayList<>();
+            for (Location location : locationsForhisMaterial) {
                 //cummulation of the stock available of this material at its locations
-                locatedStockOfThisMaterial+=location.getLocationStock();
+                locatedStockOfThisMaterial += location.getLocationStock();
             }
-            double tobelocatedSock=availableStock-locatedStockOfThisMaterial;
+            double tobelocatedSock = availableStock - locatedStockOfThisMaterial;
 
-                boolean relocationNeeded=(tobelocatedSock>0)? true:false;
-                if(relocationNeeded==true){
-                    //Map<String, Double>materialAndQuantity=new HashMap<>();
-                    //materialAndQuantity.put(material.getMatName(),tobelocatedSock);
+            boolean relocationNeeded = (tobelocatedSock > 0) ? true : false;
+            if (relocationNeeded == true) {
+                //Map<String, Double>materialAndQuantity=new HashMap<>();
+                //materialAndQuantity.put(material.getMatName(),tobelocatedSock);
 
-                    for(Location el:locationsForhisMaterial){
-                        actuellocationsnameforthematerial.add(el.getName());
-                    }
-                    String newTask=material.getMatName()+":"+tobelocatedSock+":"+actuellocationsnameforthematerial;
-                    taskListOfMaterialNeedingRelocation.add(newTask);
-
-                    model.addAttribute("title","Location needed for "+ tobelocatedSock + " of the material="+ material.getMatName());
+                for (Location el : locationsForhisMaterial) {
+                    actuellocationsnameforthematerial.add(el.getName());
                 }
-                else {//no material to be relocated
-                    model.addAttribute("title","No material to be relocated");
-                }
+                String newTask = material.getMatName() + ":" + tobelocatedSock + ":" + actuellocationsnameforthematerial;
+                taskListOfMaterialNeedingRelocation.add(newTask);
+
+                model.addAttribute("title", "Location needed for " + tobelocatedSock + " of the material=" + material.getMatName());
+
+            } else {//no material to be relocated
+                model.addAttribute("title", "No material to be relocated");
+            }
         }
-        model.addAttribute("taskListOfMaterialNeedingRelocation",taskListOfMaterialNeedingRelocation);
+        model.addAttribute("taskListOfMaterialNeedingRelocation", taskListOfMaterialNeedingRelocation);
         return "location/taskList";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String addlocationdisplayform(Model model) {
         model.addAttribute("title", "Create new location for material storage in the warehouse ");
-        model.addAttribute("location",new Location());
+        model.addAttribute("location", new Location());
         model.addAttribute(new MMaterial());
         model.addAttribute(new Flow());
-        model.addAttribute("materials",matDao.findAll());
+        model.addAttribute("materials", matDao.findAll());
 
-        model.addAttribute("availableLocations",locDao.findAll());
+        model.addAttribute("availableLocations", locDao.findAll());
 
         return "location/add";
     }
@@ -199,14 +201,44 @@ public class LocationController {
                                          @RequestParam("materialId") int materialId) {
 
         MMaterial material = matDao.findByMatId(materialId);
+        List<Location> listLocforselectedmaterial = material.getLocations();
         location.setMaterial(material);
         model.addAttribute("material", material);
         //model.addAttribute("materialId", location.getMaterial());
+        double locatableQuantityForTheSelectedMaterial = 0;
 
-        if (errors.hasErrors()) {
-            model.addAttribute("title", "Make sure the required fields are not empty");
+        //locatableQuantityForTheSelectedMaterial+=material.getLocations().forEach(loc->loc.getLocationStock());
+        for (Location loc : listLocforselectedmaterial) {
+            locatableQuantityForTheSelectedMaterial += loc.getLocationStock();
+
+        }
+        //model.addAttribute("locatableQuantityForTheSelectedMaterial",locatableQuantityForTheSelectedMaterial);
+        if (errors.hasErrors() || locationStock > locatableQuantityForTheSelectedMaterial) {
+            String quantitytostring = String.valueOf(locatableQuantityForTheSelectedMaterial);
+            String title = locationStock > locatableQuantityForTheSelectedMaterial ?
+                    "Error:the entered stock is greater than the available to be located(" +
+                            quantitytostring + ") for the selected material " +
+                            material.getMatName() + ".Please enter the stock less equals " + quantitytostring :
+                    "Make sure the required fields are not empty";
+
+            model.addAttribute("title", title);
+            model.addAttribute("materials", matDao.findAll());
             return "location/add";
         } else {
+/*            // Make sure that the user don't enter the a a quantity greater than the quantity
+            // not located(received but not yet placed in the warehouse)
+            double locatableQuantity=locatableQuantityForTheSelectedMaterial;
+            if (locationStock > locatableQuantity) {
+                String quantitytostring=Double.toString(locatableQuantity);
+                String title="Error:the entered stock is greater than the available to be located(" +
+                        quantitytostring + ") for the selected material " +
+                        material.getMatName() + ".Please enter the stock less equals "+quantitytostring;
+                model.addAttribute("title", title);
+                model.addAttribute("materials", matDao.findAll());
+                return "location/add";
+
+            } else {*/
+
 
             if (locDao.findByName(location.getName()) == null) { //The location does not exist before--> Save it
 
@@ -214,7 +246,7 @@ public class LocationController {
                 //update the list of the locations where this material is stored and save the updated material
                 model.addAttribute("newlocation", location);
 
-                List<Location>locationsforthisMaterial=new ArrayList<>();
+                List<Location> locationsforthisMaterial = new ArrayList<>();
                 material.getLocations().add(location);
 
                 locationsforthisMaterial.addAll(material.getLocations());
@@ -224,13 +256,13 @@ public class LocationController {
             } else {
                 //The location exists already. Test if the material stored on that location is the same material as
                 // new to be located or placed
-                if(locDao.findByName(location.getName()).getMaterial().getMatId()==location.getMaterial().getMatId()){
+                if (locDao.findByName(location.getName()).getMaterial().getMatId() == location.getMaterial().getMatId()) {
                     // update the material stock at this location with new quantity (of the part) of the delivered material
                     // update location : entryStock + existingStock
-                    double stockBeforeEntryLocation=locDao.findByName(location.getName()).getLocationStock();
-                    double stockAfterEntryLocation=stockBeforeEntryLocation+locationStock;
+                    double stockBeforeEntryLocation = locDao.findByName(location.getName()).getLocationStock();
+                    double stockAfterEntryLocation = stockBeforeEntryLocation + locationStock;
                     // Copy of the location in the new variable, update and save it
-                    Location tobeUpdatedLocation=locDao.findByName(location.getName());
+                    Location tobeUpdatedLocation = locDao.findByName(location.getName());
 
                     tobeUpdatedLocation.setLocationStock(stockAfterEntryLocation);
                     // Save the update
@@ -243,4 +275,22 @@ public class LocationController {
             }
         }
     }
+
+    @RequestMapping(value = "place/{materialName}", method = RequestMethod.GET)
+    public String placeMaterial(Model model, @PathVariable String materialName) {
+        MMaterial material = matDao.findByMatName(materialName);
+        List<Location> availableLocations = material.getLocations();
+        model.addAttribute("availableLocations", availableLocations);
+        model.addAttribute("material", material);
+        model.addAttribute("title", "Place a material '" + materialName + "'");
+        return "location/place";
+    }
+
+    @RequestMapping(value = "place/{materialName}", method = RequestMethod.POST)
+    public String placeMaterialpost(@RequestParam("materialName")String materialName, Model model) {
+
+
+
+    }
+}
 }
